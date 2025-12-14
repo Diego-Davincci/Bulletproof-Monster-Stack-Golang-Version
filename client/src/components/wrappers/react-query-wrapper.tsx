6b4f9f842ee3df1b1fd7bf2d50d3e06f0f9eb0a6
Diego-Mike <diego.cifuentes.dev@gmail.com>
2025-12-14 17:42:37 -0500

    Adding react-query setup

---
+++ client/src/components/wrappers/react-query-wrapper.tsx
@@ -0,0 +1,55 @@
+/*
+    React query configuration to handle API requests
+*/
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const MAX_RETRIES = 1; // Amount of retries before throwing a 5xx error to the user
+const HTTP_STATUS_TO_NOT_RETRY = [400, 401, 402, 403, 404]; // Do not
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retryDelay: 1000 * 2, // Amount of time to wait before making the next request on retry 👉 2sec
+      staleTime: 1000 * 60, // Amount of time before data is considered stale (meaning cosidered to get refetch) 👉 1min
+      refetchOnWindowFocus: false, // Do not refetch when changing between tabs and then coming back to the website
+      refetchOnMount: false, // Do not refetch when a component gets mounted
+      retry: (failureCount, error) => {
+        if (failureCount > MAX_RETRIES) {
+          return false;
+        }
+
+        // If statusCode = 4xx, abort retry
+        // @ts-ignore
+        if (HTTP_STATUS_TO_NOT_RETRY.includes(error.statusCode)) {
+        }
+
+        return true;
+      },
+    },
+  },
+  // Here we describe how to handle failed requests, statusCode = 5xx
+  queryCache: new QueryCache({
+    onError: (error) => {
+      // Server error
+      // @ts-ignore
+      if (error.statusCode >= 500) {
+      }
+
+      // User is not authorized, statusCode = 401
+      // @ts-ignore
+      if (error.statusCode === 401) {
+        // TODO: we can refresh the user's window here to redirect to login page
+      }
+    },
+  }),
+});
+
+const ReactQueryWrapper = () => {
+  return <QueryClientProvider client={queryClient}></QueryClientProvider>;
+};
+
+export default ReactQueryWrapper;
