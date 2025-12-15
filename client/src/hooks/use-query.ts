335d0b8091a07842c2d183d818a238edbb311660
Diego-Mike <diego.cifuentes.dev@gmail.com>
2025-12-15 13:06:52 -0500

    Adding custom mutation hook for global HTTP request

--- client/src/hooks/use-query.ts
+++ client/src/hooks/use-query.ts
@@ -1,7 +1,22 @@
+/*
+    Global custom query to make all types of GET Requests
+*/
+
 import { getHttpRequest } from "@/lib/api/client";
 import { useQuery } from "@tanstack/react-query";
 
-export const useGetQueryHook = ({
+/**
+ * Global custom query hook for performing API GET Requests
+ *
+ * This hook wraps `useQuery` from Tanstack Query and provides :
+ * - Centralized HTTP get requests logic
+ * - Error handling first
+ *
+ * @param queryKey - Unique query key for the request
+ * @param url - Backend API URL
+ *
+ */
+export const useGetQuery = ({
   queryKey,
   url,
 }: {
